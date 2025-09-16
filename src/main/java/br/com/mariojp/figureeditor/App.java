4fd6da6d79f79b88839e3fcc84c4f84a8affd57d
J. Henrique Mendes <h06.987654321@gmail.com>
2025-09-15 22:44:14 -0300

    refactor + feat: Definindo botão especifico para cada forma geometrica

--- src/main/java/br/com/mariojp/figureeditor/App.java
+++ src/main/java/br/com/mariojp/figureeditor/App.java
@@ -2,6 +2,7 @@ package br.com.mariojp.figureeditor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class App {
     public static void main(String[] args) {
@@ -14,8 +15,24 @@ public class App {
             frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
             DrawingPanel panel = new DrawingPanel();
+            JButton circleButton = new JButton("Circulo");
+            JButton rectangleButton = new JButton("Retângulo");
+
+            circleButton.addActionListener((ActionEvent e) -> {
+                panel.setInterfaceFactory(new BallFactory());
+                frame.setTitle("Figure — Modo: Círculos");
+            });
+            rectangleButton.addActionListener((ActionEvent e) -> {
+                panel.setInterfaceFactory(new RectangleFactory());
+                frame.setTitle("Figure — Modo: Retângulos");
+            });
+
+            JPanel buttonPanel = new JPanel();
+            buttonPanel.add(circleButton);
+            buttonPanel.add(rectangleButton);
 
             frame.setLayout(new BorderLayout());
+            frame.add(buttonPanel, BorderLayout.NORTH);
             frame.add(panel, BorderLayout.CENTER);
 
             frame.setSize(900, 600);
